8cc19677e9016549fb305799a7624e04388523ac
Gennadiy <kirin_gg@ngs.ru>
2021-02-04 23:30:41 +0300

    Add Difference, Union method.

--- Range/Program.cs
+++ Range/Program.cs
@@ -6,7 +6,29 @@ namespace Range
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Range range1 = new Range(1, 5);
+            Range range2 = new Range(2, 6);
+            Range range3 = new Range(6, 8);
+            Range range4 = new Range(2, 4);
+
+            Range range5 = range1.GeIntervalsIntersection(range2);
+            Range range6 = range1.GeIntervalsIntersection(range3);
+            Range range7 = range1.GeIntervalsIntersection(range4);
+
+            if (range5 != null)
+            {
+                Console.WriteLine(range5.ToString());
+            }
+
+            if (range6 != null)
+            {
+                Console.WriteLine(range6.ToString());
+            }
+
+            if (range7 != null)
+            {
+                Console.WriteLine(range7.ToString());
+            }
         }
     }
 }
--- Range/Range.cs
+++ Range/Range.cs
@@ -27,28 +27,75 @@ namespace Range
             return (number - From >= -epsilon) && (To - number >= -epsilon);
         }
 
-        public Range GeIntervalsIntersection(Range range)
+        public Range GetIntervalsIntersection(Range range)
         {
-            if(range == null)
+            if ((range == null) || (this.From > range.To) || (range.From > this.To))
             {
                 return null;
             }
 
-            if(range.From < this.From)
+            double beginRange = (range.From >= this.From) ? range.From : this.From;
+            double endRange = (range.To <= this.To) ? range.To : this.To;
+
+            return new Range(beginRange, endRange);
+        }
+
+        public Range[] GetIntervalsUnion(Range range)
+        {
+            Range[] result = new Range[2] { null, null };
+
+            if (range == null)
             {
-                if(range.To < this.From)
-                {
-                    // Пересечений нет
-                }
-
-                if(range.To > this.From)
-                {
-
-                }
-                return null;
+                return result;
             }
 
-            return null;
+            if (this.GetIntervalsIntersection(range) == null)
+            {
+                result[0] = this;
+                result[1] = range;
+
+                return result;
+            }
+
+            double beginRange = (range.From < this.From) ? range.From : this.From;
+            double endRange = (range.To > this.To) ? range.To : this.To;
+
+            result[0] = new Range(beginRange, endRange);
+
+            return result;
+        }
+
+        public Range[] GetIntervalsDifference(Range range)
+        {
+            Range[] result = new Range[2] { null, null };
+
+            if ((range == null)|| (this.Equals(range)))
+            {
+                return result;
+            }
+
+            Range intervalsIntersection = this.GetIntervalsIntersection(range);
+
+            if (intervalsIntersection == null)
+            {
+                result[0] = this;
+                result[1] = range;
+
+                return result;
+            }
+
+            double beginRange = (range.From < this.From) ? range.From : this.From;
+            double endRange = (range.To > this.To) ? range.To : this.To;
+
+            result[0] = new Range(beginRange, intervalsIntersection.From);
+            result[1] = new Range(intervalsIntersection.To, endRange);
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Range of numbers from {0} to {1}", this.From, this.To);
         }
     }
 }

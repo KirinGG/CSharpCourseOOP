666ad2bcf23aa3a8965740e0f1083664049493f3
Gennadiy <kirin_gg@ngs.ru>
2021-02-12 14:03:06 +0300

    Add project Vector.

---
+++ Vector/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vector = new Vector(5);
+            Console.WriteLine(vector.ToString());
+        }
+    }
+}
---
+++ Vector/Vector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vector
+{
+    class Vector
+    {
+        double[] elements;
+
+        public Vector(int n)
+        {
+            elements = new double[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                elements[i] = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{{{0}}}", String.Join(",", elements));
+        }
+    }
+}

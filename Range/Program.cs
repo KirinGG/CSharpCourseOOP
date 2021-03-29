f8b986486ac6d4a63f591f7a40eac451a3603723
Gennadiy Kirin <gennadiy.kirin@mikrokapital.com>
2021-03-29 16:22:12 +0300

    Revert "Add method ToString"
    This reverts commit 9fc205737e54edc12a51224ce4f79537b0852561.

--- Range/Program.cs
+++ Range/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Range
 {
@@ -7,27 +6,7 @@ namespace Range
     {
         static void Main(string[] args)
         {
-            Range range1 = new Range(1, 5);
-            Range range2 = new Range(2, 6);
-            Range range3 = new Range(6, 8);
-            Range range4 = new Range(2, 4);
-
-            Range range5 = range1.GeIntervalsIntersection(range2);
-            Range range6 = range1.GeIntervalsIntersection(range3);
-            Range range7 = range1.GeIntervalsIntersection(range4);
-
-            if (range5 != null)
-            {
-                Console.WriteLine(range5.ToString());
-            }
-            if (range6 != null)
-            {
-                Console.WriteLine(range6.ToString());
-            }
-            if (range7 != null)
-            {
-                Console.WriteLine(range7.ToString());
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }

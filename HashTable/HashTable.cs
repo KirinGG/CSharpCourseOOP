553d393a9d98f62560ab6792ea67466e6916a151
Gennadiy <kirin_gg@ngs.ru>
2021-04-07 00:06:26 +0300

    Correction of comments

--- HashTable/HashTable.cs
+++ HashTable/HashTable.cs
@@ -8,35 +8,43 @@ namespace HashTables
 {
     class HashTable<T> : ICollection<T>
     {
-        private List<T>[] lists;
         private const int DefaultArrayLength = 10;
-        private ModificationCounter modificationCounter;
+
+        private List<T>[] lists;
+        private int modificationsNumber;
+
+        public int ModificationsNumber
+        {
+            get
+            {
+                return modificationsNumber;
+            }
+
+            private set
+            {
+                if (modificationsNumber == int.MaxValue)
+                {
+                    modificationsNumber = 0;
+                }
+            }
+        }
 
         public int Count { get; private set; }
 
         public bool IsReadOnly => false;
 
-        public HashTable()
-        {
-            lists = new List<T>[DefaultArrayLength];
-            modificationCounter = new ModificationCounter(false);
-        }
-
-        public HashTable(int arrayLength)
+        public HashTable(int arrayLength = DefaultArrayLength)
         {
             if (arrayLength < 1)
             {
-                throw new ArgumentException("The array length must be greater than 0", nameof(arrayLength));
+                throw new ArgumentException($"The array length must be greater than 0. Array length: {arrayLength}.", nameof(arrayLength));
             }
 
             lists = new List<T>[arrayLength];
-            modificationCounter = new ModificationCounter(false);
         }
 
         public void Add(T item)
         {
-            CheckArgument(item);
-
             var index = GetIndex(item);
 
             if (lists[index] == null)
@@ -46,44 +54,75 @@ namespace HashTables
 
             lists[index].Add(item);
             Count++;
-            modificationCounter.Add();
+            ModificationsNumber++;
         }
 
         public void Clear()
         {
             Array.Clear(lists, 0, lists.Length);
-            modificationCounter.Add();
+            ModificationsNumber++;
+            Count = 0;
         }
 
         public bool Contains(T item)
         {
-            CheckArgument(item);
-
             var index = GetIndex(item);
+
+            if (lists[index] == null)
+            {
+                return false;
+            }
 
             return lists[index].Contains(item);
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            if(arrayIndex < 0 || arrayIndex >= lists.Length)
+            if (array == null)
             {
-                throw new ArgumentOutOfRangeException(nameof(arrayIndex), $"The index goes beyond the boundary [0, {lists.Length}] of the list. Current index value: {arrayIndex}.");
+                throw new ArgumentNullException(nameof(array));
             }
 
-            lists[arrayIndex].CopyTo(array, arrayIndex);
+            if (arrayIndex < 0 || arrayIndex >= array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(arrayIndex), $"The index goes beyond the boundary [0, {array.Length}] of the list. Current index value: {arrayIndex}.");
+            }
+
+            if (Count > (array.Length - arrayIndex))
+            {
+                throw new ArgumentException("The number of items in the source collection HashTable more available space from the position specified by the value of the index parameter to the end of the array destination array.");
+            }
+
+            var j = arrayIndex;
+
+            for (var i = 0; i < lists.Length; i++)
+            {
+                if (lists[i] == null)
+                {
+                    continue;
+                }
+
+                foreach (var item in lists[i])
+                {
+                    array[j] = item;
+                    j++;
+                }
+            }
         }
 
         public bool Remove(T item)
         {
-            CheckArgument(item);
-
             var index = GetIndex(item);
+
+            if (lists[index] == null)
+            {
+                return false;
+            }
 
             if (lists[index].Remove(item))
             {
                 Count--;
-                modificationCounter.Add();
+                ModificationsNumber++;
                 return true;
             }
 
@@ -92,11 +131,11 @@ namespace HashTables
 
         public IEnumerator<T> GetEnumerator()
         {
-            modificationCounter.Enable = true;
+            var currentModificationsNumber = ModificationsNumber;
 
             foreach (var list in lists)
             {
-                if (modificationCounter.Count > 0)
+                if (currentModificationsNumber != ModificationsNumber)
                 {
                     throw new InvalidOperationException("The data has been modified!");
                 }
@@ -109,11 +148,8 @@ namespace HashTables
                 foreach (T item in list)
                 {
                     yield return item;
-
                 }
             }
-
-            modificationCounter.Enable = false;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -130,16 +166,13 @@ namespace HashTables
             return new StringBuilder("[").Append(string.Join(", ", items)).Append("]").ToString();
         }
 
-        private static void CheckArgument(T item)
+        private int GetIndex(T item)
         {
             if (item == null)
             {
-                throw new ArgumentNullException(nameof(item), $"The argument cannot be null!");
+                return 0;
             }
-        }
 
-        private int GetIndex(T item)
-        {
             return Math.Abs(item.GetHashCode()) % lists.Length;
         }
     }
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -11,8 +11,15 @@ namespace HashTables
             hashTable.Add("101");
             hashTable.Add("5");
             hashTable.Add("1");
-
             Console.WriteLine(hashTable);
+
+            hashTable.Remove("7");
+            Console.WriteLine(hashTable);
+
+            string[] array = new string[hashTable.Count + 2];
+            hashTable.CopyTo(array, 2);
+            Console.WriteLine(string.Join(", ", array));
+
             hashTable.Clear();
             Console.WriteLine(hashTable);
         }

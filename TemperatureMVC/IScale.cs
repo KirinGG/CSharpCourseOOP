3d426dd96c24093b76eb76ea4fc83ea2fb6042bd
Gennadiy Kirin <gennadiy.kirin@mikrokapital.com>
2021-05-20 12:08:07 +0300

    Add new interface

---
+++ TemperatureMVC/IScale.cs
@@ -0,0 +1,14 @@
+﻿namespace Temperature
+{
+    interface IScale
+    {
+
+      /*  void RefreshView(double temperature);
+
+        void SetFrom(string measurementUnit);
+
+        void SetTo(string measurementUnit);
+
+        void SetValue(double temperature);*/
+    }
+}
